196c4fbdbb5ab2685b687eaa861a542143f0e724
Brian Takita <brian.takita@gmail.com>
2023-02-09 12:40:39 -0500

    + CloseDialogHandle,ModalDialog

---
+++ src/ui/CloseDialogHandle/index.d.ts
@@ -0,0 +1,7 @@
+import type { JSX, ParentProps } from 'solid-js'
+export function CloseDialogHandle($p:CloseDialogHandle__props_T):JSX.Element[]
+export type CloseDialogHandle__props_T = ParentProps<{
+	class?:string
+	tabindex?:string
+	onclick?:($:MouseEvent)=>void
+}>
---
+++ src/ui/CloseDialogHandle/index.jsx
@@ -0,0 +1,27 @@
+import { createMemo } from 'solid-js'
+import { Style_ } from '@ctx-core/ui-solid'
+/**
+ * @param $p{import('./index.d.ts').CloseDialogHandle__props_T}
+ * @returns {import('solid-js').JSX.Element[]}
+ */
+export function CloseDialogHandle($p) {
+	const tabindex_ = createMemo(()=>$p.tabindex || '0')
+	return [
+		<Style/>,
+		<a
+			href="."
+			class={`close CloseDialogHandle ${$p.class || ''}`}
+			tabindex={tabindex_()}
+			onclick={$=>{
+				$.preventDefault()
+				if ($p.onclick) $p.onclick($)
+			}}
+		>{$p.children || '&#10006;'}</a>
+	]
+}
+//language=CSS
+const Style = Style_(()=>`
+	.CloseDialogHandle {
+		text-decoration: none;
+	}
+`)
